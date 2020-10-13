68db45dcda582ca9c9636126e672e18c5cf458b9
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2020-10-13 21:49:01 +0530

    Create Rotate_image.cpp

---
+++ Sahil Rana/Arrays/Rotate_image.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/770/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int temp=0;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=i+1;j<matrix.size();j++){
+               swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        int a=0,b=matrix.size()-1;
+        
+        while(a!=b && a<b){
+             for(int i=0;i<matrix.size();i++)
+                 swap(matrix[i][a],matrix[i][b]);
+            a++;
+            b--;
+                 }
+         
+
+
+    }
+};
